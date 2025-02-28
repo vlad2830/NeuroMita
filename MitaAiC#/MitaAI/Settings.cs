2fbd5b0feaa6d2b805c5bb9474744524383c823c
VinerX <60321176+VinerX@users.noreply.github.com>
2025-02-28 11:06:52 +0300

    Настройки в c#?

--- MitaAiC#/MitaAI/Settings.cs
+++ MitaAiC#/MitaAI/Settings.cs
@@ -1,25 +1,71 @@
-﻿using System.Collections;
+﻿using MelonLoader;
+using System.IO;
+using System.Collections;
 using UnityEngine;
 
 namespace MitaAI
 {
+    [RegisterTypeInIl2Cpp]
     public class Settings : MonoBehaviour
     {
-        /*
-         * Тут будет загрузка настроек, причем можно будет редачить прям эксплорером  
-         * 
-        */
+        private string configPath;
+        private Hashtable settings;
 
-        // Use this for initialization
         void Start()
         {
-
+            configPath = Path.Combine(Application.persistentDataPath, "settings.json");
+            settings = new Hashtable();
+            LoadSettings();
         }
 
-        // Update is called once per frame
-        void Update()
+        void LoadSettings()
         {
+            if (File.Exists(configPath))
+            {
+                try
+                {
+                    string json = File.ReadAllText(configPath);
+                    settings = JsonUtility.FromJson<Hashtable>(json);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogError($"Error loading settings: {e}");
+                    settings = new Hashtable();
+                }
+            }
+        }
 
+        void SaveSettings()
+        {
+            try
+            {
+                //Il2CppSystem.Object ob  = settings;
+                Il2CppSystem.Collections.Generic.Dictionary<string, Il2CppSystem.Object> dict =
+                new Il2CppSystem.Collections.Generic.Dictionary<string, Il2CppSystem.Object>();
+
+                foreach (DictionaryEntry entry in settings)
+                {
+                    dict.Add(entry.Key.ToString(), (Il2CppSystem.Object)entry.Value);
+                }
+
+                string json = JsonUtility.ToJson(dict);
+                File.WriteAllText(configPath, json);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError($"Error saving settings: {e}");
+            }
+        }
+
+        public object Get(string key, object defaultValue = null)
+        {
+            return settings.ContainsKey(key) ? settings[key] : defaultValue;
+        }
+
+        public void Set(string key, object value)
+        {
+            settings[key] = value;
+            SaveSettings();
         }
     }
 }
