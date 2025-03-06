b67dd067a74b4ab3ad034baaf50371b2cdddaa87
VinerX <60321176+VinerX@users.noreply.github.com>
2025-03-07 00:30:31 +0300

    Улучшенное микро

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -1,12 +1,14 @@
 import time
 from io import BytesIO
-
+import asyncio
+import logging
 import soundfile as sf
 import numpy as np
 import speech_recognition as sr
+import sounddevice as sd
 
-
-import asyncio
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class AudioState:
@@ -17,13 +19,16 @@ class AudioState:
         self.is_playing = False
         self.lock = asyncio.Lock()
         self.vc = None
-        self.max_buffer_size = 50  # Максимум 50 фреймов в буфере
+        self.max_buffer_size = 10000
 
     async def add_to_buffer(self, data):
         async with self.lock:
             if len(self.audio_buffer) >= self.max_buffer_size:
-                self.audio_buffer.pop(0)  # Удаляем старые данные
+                self.audio_buffer.pop(0)
             self.audio_buffer.append(data.copy())
+
+
+audio_state = AudioState()
 
 
 class SpeechRecognition:
@@ -32,11 +37,12 @@ class SpeechRecognition:
     active = True
     audio_state = AudioState()
 
-    SAMPLE_RATE = 32000
-    CHUNK_SIZE = 256
+    SAMPLE_RATE = 48000
+    CHUNK_SIZE = 1024
     TIMEOUT_MESSAGE = False
     SILENCE_THRESHOLD = 0.01
-    SILENCE_DURATION = 2
+    SILENCE_DURATION = 4
+    MIN_RECORDING_DURATION = 1
 
     @staticmethod
     def receive_text():
@@ -46,38 +52,30 @@ class SpeechRecognition:
 
     @staticmethod
     def list_microphones():
-        """Возвращает список доступных микрофонов"""
         return sr.Microphone.list_microphone_names()
 
     @staticmethod
     async def handle_voice_message(recognized_text: str) -> None:
-        print(f"Распознанный текст: {recognized_text}")
+        logger.info(f"Распознанный текст: {recognized_text}")
         SpeechRecognition.user_input += f" {recognized_text}"
 
     @staticmethod
     async def live_recognition() -> None:
-        """Потоковое распознавание с выбранного микрофона"""
         recognizer = sr.Recognizer()
 
-        # Используем выбранный микрофон
         with sr.Microphone(device_index=SpeechRecognition.microphone_index, sample_rate=SpeechRecognition.SAMPLE_RATE,
                            chunk_size=SpeechRecognition.CHUNK_SIZE) as source:
-            print(f"Используется микрофон: {sr.Microphone.list_microphone_names()[SpeechRecognition.microphone_index]}")
+            logger.info(
+                f"Используется микрофон: {sr.Microphone.list_microphone_names()[SpeechRecognition.microphone_index]}")
             recognizer.adjust_for_ambient_noise(source)
-            print("Скажите что-нибудь...")
+            logger.info("Скажите что-нибудь...")
 
-            while True:
+            while SpeechRecognition.active:
                 try:
-                    if not SpeechRecognition.active:
-                        return
-
                     audio = await asyncio.get_event_loop().run_in_executor(
                         None,
                         lambda: recognizer.listen(source)
                     )
-
-                    # await SpeechRecognition.async_audio_callback(audio)
-
 
                     text = await asyncio.get_event_loop().run_in_executor(
                         None,
@@ -94,67 +92,62 @@ class SpeechRecognition:
 
                 except sr.WaitTimeoutError:
                     if SpeechRecognition.TIMEOUT_MESSAGE:
-                        print("Таймаут ожидания речи...")
+                        logger.info("Таймаут ожидания речи...")
                 except sr.UnknownValueError:
-                    ...
-                    # print("Речь не распознана")
+                    logger.info("Речь не распознана")
                 except Exception as e:
-                    print(f"Ошибка при распознавании: {e}")
+                    logger.error(f"Ошибка при распознавании: {e}")
                     break
 
-    #region Попытка2
     @staticmethod
     async def async_audio_callback(indata):
-
         async with SpeechRecognition.audio_state.lock:
-
             if SpeechRecognition.audio_state.is_playing:
                 return
 
             current_volume = np.max(np.abs(indata))
-            #print(f"Громкость: {current_volume}")  # Логирование громкости
-            current_volume = 1
-            #print(f"Текущая настройка {current_volume}  Уровень тишины {SpeechRecognition.SILENCE_THRESHOLD}")
             if current_volume > SpeechRecognition.SILENCE_THRESHOLD:
-                SpeechRecognition.audio_state.audio_buffer.append(indata.copy())
+                if not SpeechRecognition.audio_state.is_recording:
+                    SpeechRecognition.audio_state.is_recording = True
+                    logger.info("Начало записи.")
+                await SpeechRecognition.audio_state.add_to_buffer(indata)
                 SpeechRecognition.audio_state.last_sound_time = time.time()
-                SpeechRecognition.audio_state.is_recording = True
-                print("Данные добавлены в буфер.")
             elif SpeechRecognition.audio_state.is_recording:
-                print("222")
                 silence_time = time.time() - SpeechRecognition.audio_state.last_sound_time
                 if silence_time > SpeechRecognition.SILENCE_DURATION:
                     SpeechRecognition.audio_state.is_recording = False
-                    print("Запись завершена: тишина.")
-                    await SpeechRecognition.process_audio()  # Запуск обработки сразу после тишины
+                    logger.info("Запись завершена: тишина.")
+                    await asyncio.sleep(0.5)
+                    await SpeechRecognition.process_audio()
 
     @staticmethod
     async def process_audio():
         try:
             async with SpeechRecognition.audio_state.lock:
                 if len(SpeechRecognition.audio_state.audio_buffer) == 0:
-                    print("Аудиобуфер пуст!")
+                    logger.warning("Аудиобуфер пуст!")
                     return
-                # Проверка данных перед конкатенацией
+
                 non_empty_chunks = [chunk for chunk in SpeechRecognition.audio_state.audio_buffer if chunk.size > 0]
                 if not non_empty_chunks:
-                    print("Нет данных для обработки.")
+                    logger.warning("Нет данных для обработки.")
                     return
+
                 audio_data = np.concatenate(non_empty_chunks)
                 SpeechRecognition.audio_state.audio_buffer.clear()
 
                 with BytesIO() as audio_buffer:
                     sf.write(audio_buffer, audio_data, SpeechRecognition.SAMPLE_RATE, format='WAV')
                     audio_buffer.seek(0)
-                    recognized_text = SpeechRecognition.recognize_speech(audio_buffer)
+                    recognized_text = await SpeechRecognition.recognize_speech(audio_buffer)
 
                     if recognized_text:
                         await SpeechRecognition.handle_voice_message(recognized_text)
         except Exception as e:
-            print(f"Ошибка обработки аудио: {e}")
+            logger.error(f"Ошибка обработки аудио: {e}")
 
     @staticmethod
-    def recognize_speech(audio_buffer):
+    async def recognize_speech(audio_buffer):
         recognizer = sr.Recognizer()
         try:
             with sr.AudioFile(audio_buffer) as source:
@@ -165,38 +158,53 @@ class SpeechRecognition:
                 text = recognizer.recognize_google(audio, language="en-EN")
             return text
         except sr.UnknownValueError:
-            print("Не удалось распознать речь")
+            logger.error("Не удалось распознать речь")
             return None
         except sr.RequestError as e:
-            print(f"Ошибка API: {e}")
+            logger.error(f"Ошибка API: {e}")
             return None
 
-    #print(f"Размер audio_buffer: {len(SpeechRecognition.audio_state.audio_buffer)}")
     @staticmethod
     async def speach_recognition_start_async_other_system():
-        while True:
-            #SpeechRecognition.audio_state.is_playing = True
+        while SpeechRecognition.active:
             try:
-
-                if not SpeechRecognition.active:
-                    return
-
                 await SpeechRecognition.async_audio_callback(0)
-
                 await asyncio.sleep(0.25)
-            except:
-                ...
-
-    #endregion
+            except Exception as e:
+                logger.error(f"Ошибка в speach_recognition_start_async_other_system: {e}")
 
     @staticmethod
     async def speach_recognition_start_async():
-        """Асинхронный запуск распознавания с указанным микрофоном"""
         await SpeechRecognition.live_recognition()
-        #await  SpeechRecognition.speach_recognition_start_async_other_system()
 
     @staticmethod
     def speach_recognition_start(device_id: int, loop):
         SpeechRecognition.microphone_index = device_id
-        """Синхронный запуск распознавания с указанным микрофоном"""
         asyncio.run_coroutine_threadsafe(SpeechRecognition.speach_recognition_start_async(), loop)
+
+    @staticmethod
+    def start_audio_monitoring(device_id: int, loop):
+        SpeechRecognition.microphone_index = device_id
+        asyncio.run_coroutine_threadsafe(SpeechRecognition.speach_recognition_start_async(), loop)
+
+    @staticmethod
+    async def audio_monitoring():
+        with sd.InputStream(
+                callback=SpeechRecognition.async_audio_callback,
+                channels=1,
+                samplerate=SpeechRecognition.SAMPLE_RATE,
+                dtype='float32',
+                device=SpeechRecognition.microphone_index
+        ):
+            logger.info("Начинаем мониторинг аудиопотока...")
+            while SpeechRecognition.active:
+                await asyncio.sleep(0.1)
+
+
+async def main():
+    speech_recognition = SpeechRecognition()
+    await speech_recognition.audio_monitoring()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
