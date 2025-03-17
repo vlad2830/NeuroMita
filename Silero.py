60db3c1058f3b14a13b37c2200775a8dc01cecfe
dn-dz <admin@persona.pp.ua>
2025-03-17 22:37:07 +0200

    Update Silero.py
    Таймер ТГ

--- Silero.py
+++ Silero.py
@@ -29,12 +29,8 @@ class TelegramBotHandler:
         self.patch_to_sound_file = ""
         self.last_speaker_command = ""
 
-        print(f"DEBUG. TG TIME LIMIT BEFORE IS: {self.silero_time_limit}")
-
-        if self.silero_time_limit is None:
+        if not hasattr(self, "silero_time_limit") or self.silero_time_limit is None:
             self.silero_time_limit = 10
-
-        print(f"DEBUG. TG TIME LIMIT AFTER IS: {self.silero_time_limit}")
 
         if getattr(sys, "frozen", False):
             # Если программа собрана в exe, получаем путь к исполняемому файлу
@@ -165,11 +161,11 @@ class TelegramBotHandler:
         print("Ожидание ответа от бота...")
         response = None
         attempts = 0
-        attempts_per_second = 4
-        print("DEBUG. TG TIME LIMIT IS:", self.silero_time_limit)
+        attempts_per_second = 2
+
         attempts_max = self.silero_time_limit * attempts_per_second
 
-        await asyncio.sleep(0.7)
+        await asyncio.sleep(2)
         while attempts <= attempts_max:  # Попытки получения ответа
 
             async for message in self.client.iter_messages(self.tg_bot, limit=1):
