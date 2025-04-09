dcc10c13ee1afc9b74bc24b6b3430b3454f8a801
VinerX <60321176+VinerX@users.noreply.github.com>
2025-04-09 19:57:53 +0300

    Немного переноса

--- MitaAiC#/MitaAI/WorldModded/RoomControl.cs
+++ MitaAiC#/MitaAI/WorldModded/RoomControl.cs
@@ -1,5 +1,12 @@
-﻿namespace MitaAI
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MitaAI
 {
+
     public enum Rooms
     {
         Kitchen = 0,
